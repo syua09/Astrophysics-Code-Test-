1ac1bc13d7677412cfcbdb12e48de1af9f274710
syua09 <158415822+syua09@users.noreply.github.com>
2024-02-23 15:28:35 -0800

    Add files via upload

--- Astrophysics Graph (Laptop).py
+++ Astrophysics Graph (Laptop).py
@@ -8,20 +8,23 @@ from astropy import wcs
 import matplotlib.pyplot as plt
 import photoutils as photo
 
-Star = fits.open('Star(0).fits')
+Star = fits.open('Star(1).fits')
 Header = (Star[0].header)
 Data = Star[0].data
 
-Row_15 = pd.DataFrame(Data[15])
+y = Data[15]
+x = [1 , 2 , 3 ,4 ,5 ,6 ,7 ,8 , 9 ,10 , 11 ,12,13 , 14 ,15 ,16, 17 , 18, 19, 20 ,21, 22 ,23 ,24 ,25 ,26 , 27 , 28, 29, 30 , 31]
 
-print(Row_15)
+print(len(x) , len(y))
+
+plt.scatter(x , y)
+plt.plot(x , y)
 
 
+# for i in range (29) :
+#     plt.scatter(i, Row_15)
 
-# for i in range (15) :
-#     plt.scatter(Data[i], Data[15])
-
-    #print(Data[1 , i] , i)
+    
 
 
 plt.show()
